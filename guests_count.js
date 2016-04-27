edb3767f255b7e39deaf0efe4ee87c269c3b38a1
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2016-04-27 13:17:29 +0000

    Ask user to relogin if his session is expired

--- guests_count.js
+++ guests_count.js
@@ -1,57 +1,61 @@
 define(function(require, exports, module) {
-    main.consumes = ['Plugin', 'ui', 'layout', 'api', 'info', 'menus'];
-    main.provides = ['ethergit.guests.count'];
-    return main;
+  main.consumes = ['Plugin', 'ui', 'layout', 'api', 'info', 'menus', 'login'];
+  main.provides = ['ethergit.guests.count'];
+  return main;
 
-    function main(options, imports, register) {
-        var Plugin = imports.Plugin;
-        var ui = imports.ui;
-        var layout = imports.layout;
-        var api = imports.api;
-        var info = imports.info;
-        var menus = imports.menus;
-        
-        var plugin = new Plugin('Ethergit', main.consumes);
-        var emit = plugin.getEmitter();
+  function main(options, imports, register) {
+    var Plugin = imports.Plugin;
+    var ui = imports.ui;
+    var layout = imports.layout;
+    var api = imports.api;
+    var info = imports.info;
+    var menus = imports.menus;
+    var login = imports.login;
+    
+    var plugin = new Plugin('Ethergit', main.consumes);
+    var emit = plugin.getEmitter();
 
-        var label;
-        
-        function load() {
-            label = ui.insertByIndex(
-                layout.getElement('barExtras'),
-                new ui.label({
-                    id: 'guestCount',
-                    caption: '0',
-                    class: 'guest-count',
-                    tooltip: 'Guests number'
-                }),
-                650, plugin
-            );
-            ui.insertCss(require('text!./style.css'), false, plugin);
-            update();
-            setInterval(update, 10000);
-        }
-
-        function update() {
-            api.collab.get('guest-count', function(err, data) {
-                if (err) return console.error(err);
-                label.setAttribute('caption', data.count);
-            });
-        }
-        
-        plugin.on('load', function() {
-            load();
-        });
-        plugin.on('unload', function() {
-        
-        });
-        
-        plugin.freezePublicAPI({
-            
-        });
-        
-        register(null, {
-            'ethergit.guests.count': plugin
-        });
+    var label;
+    
+    function load() {
+      label = ui.insertByIndex(
+        layout.getElement('barExtras'),
+        new ui.label({
+          id: 'guestCount',
+          caption: '0',
+          class: 'guest-count',
+          tooltip: 'Guests number'
+        }),
+        650, plugin
+      );
+      ui.insertCss(require('text!./style.css'), false, plugin);
+      update();
+      setInterval(update, 10000);
     }
+
+    function update() {
+      api.collab.get('guest-count', function(err, data) {
+        if (err) {
+          if (err.code == 404) login.relogin();
+          return console.error(err);
+        }
+        label.setAttribute('caption', data.count);
+      });
+    }
+    
+    plugin.on('load', function() {
+      load();
+    });
+    plugin.on('unload', function() {
+      
+    });
+    
+    plugin.freezePublicAPI({
+      
+    });
+    
+    register(null, {
+      'ethergit.guests.count': plugin
+    });
+  }
 });
